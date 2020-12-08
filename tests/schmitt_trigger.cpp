175f4c53e71794eed294e899ab299f01dd1074d6
Stefan Hagen <stefan@hagen.link>
2020-12-08 18:28:07 +0100

    Create schmitt_trigger.cpp

---
+++ tests/schmitt_trigger.cpp
@@ -0,0 +1,10 @@
+#include "catch.hpp"
+#include "../schmitt/trigger.hpp"
+
+TEST_CASE("Cycling (once)", "[positive]") {
+    auto const low = -1.f, high = 2.f;
+    bool const default_out = false;
+    auto st = schmitt::trigger<float>(low, high, default_out);
+    CHECK(!st(0.f));
+    REQUIRE(st(high + 1.f));
+}
